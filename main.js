25d3b4a3ff06c8c270d4a7683fee8c0305f4484f
Will McGIll <willmcgill@Wills-MacBook-Pro.local>
2019-09-13 18:22:39 -0400

    added third case

--- main.js
+++ main.js
@@ -11,20 +11,30 @@ var circleButton = document.getElementById('circleButton');
 function render(){
     console.log('render function');
 
-    switch(stateArray[1]){
+    switch(stateArray[2]){
         case "start":
             console.log('start');
             largeText.innerHTML = 'I can read your mind';
             squareButton.style.display = "none";
             smallText.style.display = "none";
             circleButton.innerHTML = 'Start';
+            break;
 
         case "pick":
             console.log('pick');
             largeText.innerHTML = 'Pick a number between 1 and 99';
             squareButton.innerHTML = 'Next';
-            smallText.innerHTML = 'Add both digits to get a new number';
+            smallText.innerHTML = 'When you have your number click next';
             circleButton.innerHTML = 'Back';
+            break;
+
+        case "add":
+            console.log('add');
+            largeText.innerHTML = 'Add both digits to get a new number';
+            squareButton.innerHTML = 'Next';
+            smallText.innerHTML = 'Ex: 14 is 1 + 4 = 5  click next to proceed';
+            circleButton.innerHTML = 'Back';
+
     }
 
         
